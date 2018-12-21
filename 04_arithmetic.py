09b74c91405b22a8614e80ab1a4ed3e0e5fb3177
Clayton Bauman <claytonkb@gmail.com>
2018-12-20 16:34:59 -0800

    Misc changes

--- 04_arithmetic.py
+++ 04_arithmetic.py
@@ -13,6 +13,26 @@ import numpy as np
 #        [ 1.0000, -1.0000]])
 #>>> torch.tensor(np.array([[1, 2, 3], [4, 5, 6]]))
 
+x = torch.rand(3,1)
+y = torch.rand(3,1).transpose(0,1)
+
+z = torch.matmul(x, y)
+print("z=x*y")
+print(z.numpy())
+
+w = torch.matmul(y, x)
+print("w=y*x")
+print(w.numpy())
+
+# Use transpose(0,1) to perform an ordinary matrix transpose. Only works on
+# 2d tensors. You can get fancy with this method, but clearer to just select
+# a 2d matrix from your n-dim tensor then perform a standard 2d transpose on
+# it.
+
+print("z^T")
+print(z.transpose(0,1).numpy())
+
+
 a = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float64)
 print("a")
 print(a)
@@ -22,6 +42,7 @@ tensor_f64 = torch.tensor((), dtype=torch.float64)
 b = tensor_f64.new_full((2,3), 2.71828)
 print("b")
 print(b)
+
 
 # element-wise sum
 c = b.add(a)
@@ -51,4 +72,7 @@ print(g)
 h = torch.dot(f, g)
 print("h=dot(f,g)")
 print(h)
+
+
+
 
