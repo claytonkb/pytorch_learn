855c1d97b70c0b3c6e6c2253061aae93ae687ac5
Clayton Bauman <claytonkb@gmail.com>
2018-12-21 10:35:45 -0800

    Added URLs

--- 05_gradients.py
+++ 05_gradients.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 
 #https://explained.ai/matrix-calculus/index.html
+#http://neuralnetworksanddeeplearning.com/chap2.html
+#https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
 
 print("gradients")
 
